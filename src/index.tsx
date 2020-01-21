578dfc55d887035541b2c08ae0665cca84c39b95
Zelenovsky, Ivan (iz1155) <iz1155@intl.att.com>
2020-01-21 17:04:00 +0200

    PWA Init

--- src/index.tsx
+++ src/index.tsx
@@ -11,11 +11,11 @@ import store from './store/store';
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>, 
+    </Provider>,
     document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
