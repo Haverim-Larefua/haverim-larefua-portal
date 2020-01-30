211daf40958b82a012aeb77e4778c1ac9bb79d52
Zelenovsky, Ivan (iz1155) <iz1155@intl.att.com>
2020-01-30 10:55:24 +0200

    Nav fix

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -8,9 +8,9 @@ const Header: React.FC = () => {
     <div className="fhh-header">
       <div className="fhh-header__logo">חברים לרפואה</div>
       <nav className="fhh-header__nav">
-        <li><Link className="fhh-nav__item" to='/admin' >מבט על</Link></li>
-        <li><Link className="fhh-nav__item" to='/parcels'> חבילות</Link></li>
-        <li><Link className="fhh-nav__item--selected" to='/users' > שליחים</Link></li>
+        <Link className="fhh-nav__item" to='/admin' >מבט על</Link>
+        <Link className="fhh-nav__item" to='/parcels'> חבילות</Link>
+        <Link className="fhh-nav__item--selected" to='/users' > שליחים</Link>
       </nav>
       <div className="fhh-header__user">
 
