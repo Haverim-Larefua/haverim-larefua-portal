eefca721f491882f6bd6ccc71a0cec57772c5010
Zelenovsky, Ivan (iz1155) <iz1155@intl.att.com>
2020-02-02 10:03:07 +0200

    Display Active route in navigation menu

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 import './Header.scss';
 const Header: React.FC = () => {
+
   return (
     <div className="fhh-header">
       <div className="fhh-header__logo">חברים לרפואה</div>
       <nav className="fhh-header__nav">
-        <Link  className="fhh-nav__item" to='/admin' >מבט על</Link>
-        <Link  className="fhh-nav__item" to='/parcels'> חבילות</Link>
-        <Link  className="fhh-nav__item--selected" to='/users' > שליחים</Link>
+        <NavLink activeClassName="fhh-nav__item--active "  className="fhh-nav__item" to='/admin' >מבט על</NavLink>
+        <NavLink activeClassName="fhh-nav__item--active " className="fhh-nav__item" to='/parcels'> חבילות</NavLink>
+        <NavLink activeClassName="fhh-nav__item--active "  className="fhh-nav__item" to='/users' > שליחים</NavLink>
       </nav>
       <div className="fhh-header__user">
 
