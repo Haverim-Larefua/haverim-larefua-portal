1b286e73d640303eae7c4d6fcf835002bbc13c11
Simhi, Yuval <yuval.simhi@intl.att.com>
2020-08-16 13:33:55 +0300

    created download app page

--- src/components/ApplicationDownloadPage/ApplicationDownloadPage.tsx
+++ src/components/ApplicationDownloadPage/ApplicationDownloadPage.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { withRouter } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../assets/icons/logo.svg';
-
+import { ReactComponent as BlueLogo } from "../../assets/icons/login-image-flat.svg";
+import { ReactComponent as Download } from "../../assets/icons/download.svg";
 import './ApplicationDownloadPage.scss';
 import { Button } from "../shared/Button/Button";
 import httpService from "../../services/http";
-
+ 
 const ApplicationDownloadPage: React.FC = () => {
 
   const downloadApp = async () => {
@@ -16,13 +16,13 @@ const ApplicationDownloadPage: React.FC = () => {
     <div className="application-download-page">
 
       <div className="container">
-        <div className="logo-container"> <Logo /> </div>
+        <div className="logo-container"> <BlueLogo /> </div>
 
         <div className="application-download-page_text">
           אפליקציית המתנדבים של עמותת חברים לרפואה
       </div>
 
-        <Button className="application-download-page_button" onClick={downloadApp}>להורדת האפליקציה</Button>
+        <Button className="application-download-page_button" onClick={downloadApp}><Download className="download" /> להורדת האפליקציה</Button>
       </div>
     </div>
   );
