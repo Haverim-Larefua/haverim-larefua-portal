821ffd1a699687db3c0cca0a3c4c7f6831a4a1d1
Lev <yl9209@intl.att.com>
2020-10-22 17:46:52 +0300

    Fix rows selection in "packages" page

--- src/components/Parcels/Parcels.jsx
+++ src/components/Parcels/Parcels.jsx
@@ -199,6 +199,8 @@ const Parcels = () => {
         onSelectedRowsChange={onSelectedRowsChanged}
         subHeaderComponent={buildToolBar()}
         selectableRows
+        selectedRowsState={selectedRowsState}
+        selectedRowIdentifierKey="id"
         pointerOnHover
         noDataComponent={parcelExtendedData.error ? AppConstants.serverErrorMessage : noDataMessage}
         loading={!parcelExtendedData.parcels && !parcelExtendedData.error}
